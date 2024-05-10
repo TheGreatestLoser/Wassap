34cc1b7c160040a7a801c2cdaeb1db17787b26b7
Vergil <semenovtemirkan78951984@gmail.com>
2024-05-10 10:38:20 +0300

    Add minus func

--- bri/gg.py
+++ bri/gg.py
@@ -2,5 +2,10 @@ def plus(a: int, b: int) -> int:
     return a + b
 
 
+def minus(a: int, b: int) -> int:
+    return a - b
+
+
 if __name__ == "__main__":
     print(plus(1, 2))
+    print(minus(10, 5))
